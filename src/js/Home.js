5dd57dc3fa4be1da14f86923448e5f109b57640a
OnyJ <julien.josselin75@hotmail.fr>
2020-04-29 19:34:14 +0200

    research functionnal

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,48 +1,70 @@
 const URL_MOST_WANTED_GAMES =
   "https://api.rawg.io/api/games?dates=2020-01-01,2021-10-10&ordering=-added";
-// const URL = "https://api.rawg.io/api/games";
+const SEARCH_URL = "https://api.rawg.io/api/games?search=";
 
 const Home = (argument = "") => {
   console.log("Page Home", argument);
-  // <h1 style="color: red;">Le menu stp</h1>;
+
+  const doFetch = (finalURL) => {
+    let articles = "";
+
+    fetch(`${finalURL}`)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("ma response");
+        console.log(response);
+        response.results.forEach((article) => {
+          articles += `
+              <div class="cardGame">
+                <h1>${article.name}</h1>
+                <h2>${article.released}</h2>
+                <a href = "#gamedetail/${article.id}">${article.id}</a>
+              </div>
+            `;
+        });
+        document.querySelector(".game-list .articles").innerHTML = articles;
+      });
+  };
 
   const preparePage = () => {
     // let cleanedArgument = argument.replace(/\s+/g, "-");
-    let cleanedArgument = "?dates=2020-01-01,2021-10-10&ordering=-added";
+    // let cleanedArgument = "?dates=2020-01-01,2021-10-10&ordering=-added";
     let articles = "";
 
     const fetchList = (url, argument) => {
       let finalURL = url;
-      // if (argument) {
+      // if (argument != "") {
       // finalURL = URL + "?search=" + argument;
       // }
-
-      fetch(`${finalURL}`)
-        .then((response) => response.json())
-        .then((response) => {
-          console.log(response);
-          response.results.forEach((article) => {
-            articles += `
-                  <div class="cardGame">
-                    <h1>${article.name}</h1>
-                    <h2>${article.released}</h2>
-                    <a href = "#gamedetail/${article.id}">${article.id}</a>
-                  </div>
-                `;
-          });
-          document.querySelector(".game-list .articles").innerHTML = articles;
-        });
+      doFetch(finalURL);
     };
-    fetchList(URL_MOST_WANTED_GAMES, cleanedArgument);
+    fetchList(URL_MOST_WANTED_GAMES);
+  };
+
+  const searchGame = () => {
+    console.log("searching");
+    let argument = document.getElementsByTagName("input")[0].value;
+    let cleanedArgument = argument.replace(/\s+/g, "-");
+    console.log(SEARCH_URL + cleanedArgument);
+    doFetch(SEARCH_URL + cleanedArgument);
   };
 
   const render = () => {
     pageContent.innerHTML = `
       <section class="game-list">
+        <div class="search-bar">
+          <form>
+            <input type="text" />
+            <input type="submit" id="submit-search" />
+          </form>
+        </div>
         <div class="articles">Hey, this page is a GameList template, about : ${argument}</div>
       </section>
     `;
     preparePage();
+    document
+      .getElementById("submit-search")
+      .addEventListener("click", searchGame);
   };
 
   render();
