9021121d358b55d03afd56fdd9d6a055f8a3cff2
OnyJ <julien.josselin75@hotmail.fr>
2020-04-30 14:19:40 +0200

    select platform is done

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,3 +1,4 @@
+let selectedPlatform = "";
 const URL_MOST_WANTED_GAMES =
   "https://api.rawg.io/api/games?dates=2020-01-01,2021-10-10&ordering=-added";
 
@@ -7,32 +8,70 @@ const Home = (argument = "") => {
   const preparePage = (finalURL) => {
     let articles = "";
 
+    const selectByPlatform = (hardArticlePlatforms) => {
+      let articlePlatforms = [];
+      hardArticlePlatforms.forEach((platform) => {
+        articlePlatforms.push(platform.platform.slug);
+      });
+      console.log;
+      console.log(articlePlatforms);
+      if (selectedPlatform === "") return true;
+      return articlePlatforms.includes(selectedPlatform) ? true : false;
+    };
+
+    // const findplatform = (platforms) => {
+    // platforms.forEach((platform) => {
+    // console.log(platform.platform.slug);
+    // });
+    // console.log(article.platforms[0].platform.slug);
+    // };
+
     fetch(`${finalURL}`)
       .then((response) => response.json())
       .then((response) => {
         console.log("ma response");
         console.log(response);
         response.results.forEach((article) => {
-          articles += `
+          if (selectByPlatform(article.platforms)) {
+            // console.log(article.platforms[0].platform.slug);
+
+            articles += `
               <div class="cardGame">
                 <h1>${article.name}</h1>
                 <h2>${article.released}</h2>
                 <a href = "#gamedetail/${article.id}">${article.id}</a>
               </div>
             `;
+          }
         });
         document.querySelector(".game-list .articles").innerHTML = articles;
       });
   };
 
+  const changePlatform = () => {
+    selectedPlatform = document.getElementById("filter").value;
+    preparePage(URL_MOST_WANTED_GAMES);
+  };
+
   const render = () => {
     pageContent.innerHTML = `
       <section class="game-list">
-        <!-- here was the search bar -->
+        <select id="filter">
+          <option value="">Platforms</option>
+          <option value="pc">PC</option>
+          <option value="xbox-one">Xbox One</option>
+          <option value="playstation4">Playstation 4</option>
+          <option value="nintendo-switch">Nintendo Switch</option>
+          <option value="ios">iOS</option>
+          <option value="android">Android</option>
+          <option value="macos">Mac OS</option>
+          <option value="linux">Linux</option>
+        </select>
         <div class="articles">Hey, this page is a GameList template, about : ${argument}</div>
       </section>
     `;
     preparePage(URL_MOST_WANTED_GAMES);
+    document.getElementById("filter").addEventListener("click", changePlatform);
   };
   render();
 };
